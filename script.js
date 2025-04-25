20337f78fb1f973b478b63a0fdfb16548797fba6
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-04-25 05:48:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // your code here
+
+let nameSec = document.querySelector("#name");
+let yearSec = document.querySelector("#year");
+let btn = document.querySelector("#button");
+let url = document.querySelector("#url");
+
+const value = url.innerText;
+
+btn.addEventListener("click",()=>{
+	if(nameSec.value.trim() === "" && yearSec.value.trim() !== ""){
+		url.innerText = `${value}?year=${yearSec.value}`;
+	}else if(nameSec.value.trim() !== "" && yearSec.value.trim() === ""){
+		url.innerText = `${value}?name=${nameSec.value}`;
+	}else{
+		url.innerText = `${value}?name=${nameSec.value}&year=${yearSec.value}`;
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
